214222f7d2c23ef0a312cc100995c3de8e596440
Bhagyesh Patel <Bhagydeep@BhagyessMacBook.fios-router.home>
2018-07-22 15:58:45 -0400

    Final Output

--- Project 2/Project 2 - Titanic.R
+++ Project 2/Project 2 - Titanic.R
@@ -5,3 +5,24 @@ library(tidyr)
 titanic_orginal <- read.csv("titanic3.csv")
 titanic_orginal
 summary (titanic_orginal)
+
+#
+titanic_orginal <-
+  titanic_orginal %>%
+  mutate (embarked = replace(embarked, is.na(embarked), "S"))
+
+#
+titanic_orginal <-
+  titanic_orginal %>%
+  mutate (age = replace(age, is.na(age), mean (as.numeric(age), na.rm = TRUE)))
+
+#
+titanic_orginal <-
+  titanic_orginal %>%
+  mutate (boat = replace(boat, is.na(boat), "None"))
+
+#
+titanic_orginal <-
+  titanic_orginal %>%
+  mutate(has_cabin_number = ifelse(!is.na(cabin), 1, 0 ))
+         
