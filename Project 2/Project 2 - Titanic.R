41b232b597bbe0f85f7085d86dbf4913c38630f3
Bhagyesh Patel <Bhagydeep@BhagyessMacBook.fios-router.home>
2018-07-22 15:02:19 -0400

    New File

---
+++ Project 2/Project 2 - Titanic.R
@@ -0,0 +1,7 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
+titanic_orginal <- read.csv("titanic3.csv")
+titanic_orginal
+summary (titanic_orginal)
--- README.md
+++
@@ -1,5 +0,0 @@
-# mini-data-science-projects-with-R-
-library(dplyr)
-summary(refine)
-str(refine)
-select(refine, company)
