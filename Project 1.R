020cbaac22236bd8747377ea49427b4a4f5550ae
Bhagyesh Patel <Bhagydeep@BhagyessMacBook.fios-router.home>
2018-07-22 11:18:42 -0400

    Changes

--- Project 1.R
+++ Project 1.R
@@ -1,10 +1,17 @@
 library(dplyr)
+library(readr)
+
+refine <- read_csv("refine_original.csv")
+
 summary(refine)
 str(refine)
 select(refine, company)
 select(refine, company(starts_with("philp")))
+grepl()
 
-clean_company <- refine %>% mutate(company = tolower(company)) %>% 
+clean_company <- 
+  refine %>% 
+  mutate(company = tolower(company)) %>% 
   mutate(company = gsub("phillips|phllips|phillps|fillips|phlips", "philips", x=company)) %>%
   mutate(company = gsub("akz0|ak zo", "akzo", x=company)) %>% 
   mutate(company = gsub("unilver", "unilever", x=company))
