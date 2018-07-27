91ee6dd493cd5f28dc7afa5e749adb683002f0c0
Bhagyesh <patel.bhagydeep@gmail.com>
2018-07-27 15:57:39 -0400

    With ggplot2

--- Project 2/Project 2 - Titanic.R
+++ Project 2/Project 2 - Titanic.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 titanic_orginal <- read_csv("Project 2/titanic3.csv")
 titanic_orginal
@@ -34,3 +35,22 @@ View(titanic_orginal)
 # Loading Final Output to Output_titanic
 write_csv(titanic_orginal, "Project 2/Output_titanic.csv")
 
+# 1 - Check the structure of titanic
+str(titanic3)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic3, aes(x = pclass, fill = sex)) +
+  geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic3, aes(x = pclass, fill = sex)) +
+  geom_bar(position = "dodge") +
+  facet_grid(.~ survived)
+
+# 4 - Define an object for position jitterdodge, to use below
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
+
+# 5 - Plot 3, but use the position object from instruction 4
+ggplot(titanic3, aes(x = pclass, y = age, col = sex)) +
+  geom_point(position = posn.jd, size = 3, alpha = 0.5) +
+  facet_grid(.~ survived)
